7b3f4bd29723b6a00d1c9616968e7f0b1a60ead9
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-04-24 21:08:18 -0500

    make index and show actions

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,14 @@
 class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+    render 'index.html.erb'
+  end
+
+  def show
+    @pet = Pet.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  get '/pets' => 'pets#index'
+  post '/pets/new' => 'pets#new'
+  get '/pets/:id' => 'pets#show'
 end
